cf4e0ced371ad9cbe92bce8a571c4a282f14b59f
smallerz <smallerz@proton.me>
2025-02-09 20:26:22 +0000

    Add basic CLI arguments for testing purposes
    - Add arguments to load a basic binary file
      (not ELF) and set the emulator's memory
      for testing purposes.
    
    - Update emulator logic so that it receives
      instruction data as an array of bytes.
    
    - Add sample binary files for the emulator
      to read.

--- lib/cli/src/main.rs
+++ lib/cli/src/main.rs
@@ -1,18 +1,45 @@
+use std::{fs::File, io::{Error, Read}, process};
+
+use clap::Parser;
+
 use riscv_emulator::emulator::{
     Emulator,
     EmulatorConfig,
 };
 
+#[derive(Debug, Parser)]
+struct Args {
+    /// The RISC-V ELF binary to execute
+    #[arg()]
+    input_file: String,
+
+    /// The size of the emulator's memory in bytes
+    #[arg(short, long, default_value_t = 1024)]
+    memory_size: usize,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let config = EmulatorConfig { 
-        mem_size: 1024,
+        mem_size: args.memory_size,
         proc_count: 1,
     };
 
     let mut emu = Emulator::build(config);
 
-    println!("Memory (B):\t{}", emu.memory.len());
-    println!("Processors:\t{}", emu.proc.len());
+    let data = dev_read_input_file(&args.input_file)
+        .unwrap_or_else(|err| {
+            eprintln!("Error: {err}");
+            process::exit(1);
+        });
 
-    emu.debug();
+    emu.dev_start(&data);
+}
+
+fn dev_read_input_file(path: &str) -> Result<Vec<u8>, Error> {
+    let file = File::open(path);
+    let mut buf = Vec::new();
+    file?.read_to_end(&mut buf)?;
+    Ok(buf)
 }
