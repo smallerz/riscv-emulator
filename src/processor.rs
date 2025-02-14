13681d235508297f00eb31b0393d9641ac731d10
smallerz <smallerz@proton.me>
2025-02-14 01:28:16 +0000

    Consolidate operation types in processor

--- src/processor.rs
+++ src/processor.rs
@@ -60,17 +60,36 @@ impl Processor {
     /// Executes an instruction.
     pub fn execute(&mut self, instr: &Instruction) {
         match Decoder::decode(instr) {
-            Some(ArithmeticAdd)                 => self.exec_add(instr),
-            Some(ArithmeticAddImmediate)        => self.exec_addi(instr),
-            Some(ArithmeticSub)                 => self.exec_sub(instr),
-            Some(LoadUpperImmediate)            => todo!(),
-            Some(LogicalAnd)                    => self.exec_and(instr),
-            Some(LogicalAndImmediate)           => self.exec_andi(instr),
-            Some(LogicalExclusiveOr)            => self.exec_xor(instr),
-            Some(LogicalExclusiveOrImmediate)   => self.exec_xori(instr),
-            Some(LogicalOr)                     => self.exec_or(instr),
-            Some(LogicalOrImmediate)            => self.exec_ori(instr),
-            _                                   => self.handle_illegal_instr(instr),
+            // Instructions that perform register-immediate operations.
+            op @ Some(
+                ArithmeticAddImmediate 
+                | LogicalAndImmediate
+                | LogicalExclusiveOrImmediate
+                | LogicalOrImmediate
+            ) => {
+                self.exec_alu_op_i(
+                    &op.unwrap(), 
+                    instr
+                );
+            },
+
+            // Instructions that perform register-register operations.
+            op @ Some(
+                ArithmeticAdd
+                | ArithmeticSub
+                | LogicalAnd
+                | LogicalExclusiveOr
+                | LogicalOr
+            ) => {
+                self.exec_alu_op_r(
+                    &op.unwrap(), 
+                    instr
+                );
+            },
+
+            Some(LoadUpperImmediate) => todo!(),
+
+            _ => self.handle_illegal_instr(instr),
         }
     }
 
@@ -93,100 +112,6 @@ impl Processor {
                 self.reg_x.read(instr.rs1().unwrap()) as i32,
                 instr.imm().unwrap(),
             ) as u32
-        );
-    }
-
-    /// Executes an `add` instruction.
-    fn exec_add(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            .wrapping_add_signed(self.reg_x.read(instr.rs2().unwrap()) as i32);
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
-        );
-    }
-
-    /// Executes an `addi` instruction.
-    fn exec_addi(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            .wrapping_add_signed(instr.imm().unwrap());
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
-        );
-    }
-
-    fn exec_and(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            & self.reg_x.read(instr.rs2().unwrap());
-
-        self.reg_x.write(
-            instr.rd().unwrap(), 
-            result,
-        );
-    }
-
-    fn exec_andi(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            & instr.imm().unwrap() as u32;
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
-        );
-    }
-
-    fn exec_or(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            | self.reg_x.read(instr.rs2().unwrap());
-
-        self.reg_x.write(
-            instr.rd().unwrap(), 
-            result,
-        );
-    }
-
-    fn exec_ori(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            | instr.imm().unwrap() as u32;
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
-        );
-    }
-
-    fn exec_xor(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            ^ self.reg_x.read(instr.rs2().unwrap());
-
-        self.reg_x.write(
-            instr.rd().unwrap(), 
-            result,
-        );
-    }
-
-    fn exec_xori(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            ^ instr.imm().unwrap() as u32;
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
-        );
-    }
-
-    fn exec_sub(&mut self, instr: &Instruction) {
-        let result = self.reg_x.read(instr.rs1().unwrap())
-            .wrapping_sub(
-                self.reg_x.read(instr.rs2().unwrap())
-            );
-
-        self.reg_x.write(
-            instr.rd().unwrap(),
-            result,
         );
     }
 
